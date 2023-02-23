39cc68a3cfcc301b052ae826d78f2b032e42ad4f
aufamuhammad715 <aufamuhammad715@gmail.com>
2023-02-23 19:48:59 +0700

    Adding ref

---
+++ test/ref.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "etl/ref.h"
+
+using namespace Project::etl;
+
+TEST(Ref, Ref) {
+    struct Struct {
+        int a, b;
+        constexpr explicit Struct(int a = 10, int b = 11) : a(a), b(b) {}
+    };
+    constexpr ConstRef<Struct> s = nullptr;
+    constexpr ConstRef<Struct> d = Struct{12, 13};
+
+    EXPECT_EQ(s->a, 10);
+    EXPECT_EQ(s->b, 11);
+    EXPECT_EQ(d->a, 12);
+    EXPECT_EQ(d->b, 13);
+}
