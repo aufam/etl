9c29c003944e89daf47123d685726de1d6a2a2f2
aufam <aufamuhammad715@gmail.com>
2023-08-15 16:17:04 +0700

    adding getter_setter

---
+++ include/etl/getter_setter.h
@@ -0,0 +1,62 @@
+#ifndef ETL_GETTER_SETTER_H
+#define ETL_GETTER_SETTER_H
+
+#include "etl/utility_basic.h"
+
+namespace Project::etl {
+
+    template <typename T, typename GetterFunction>
+    class Getter {
+        GetterFunction get;
+
+    public:
+        constexpr Getter(GetterFunction get) : get(get) {}        
+      
+        constexpr operator T() const { return get(); }
+    };
+    
+
+    template <typename T, typename SetterFunction>
+    class Setter {
+        SetterFunction set;
+
+    public:
+        constexpr Setter(SetterFunction set) : set(set) {}
+
+        template <typename U>
+        constexpr decltype(auto) operator=(U&& value) const { return set(etl::forward<U>(value)); }
+    };
+
+
+    template <typename T, typename GetterFunction, typename SetterFunction>
+    class GetterSetter {
+        GetterFunction get;
+        SetterFunction set;
+
+    public:
+        constexpr GetterSetter(GetterFunction get, SetterFunction set) : get(get), set(set) {}
+
+        constexpr operator T() const { return get(); }
+
+        template <typename U>
+        constexpr decltype(auto) operator=(U&& value) const { return set(etl::forward<U>(value)); }
+    };
+
+
+    template <typename T, typename GetterFunction> constexpr auto
+    make_getter(GetterFunction&& get) { return Getter<T, GetterFunction> { etl::forward<GetterFunction>(get) }; }
+
+    template <typename T, typename SetterFunction> constexpr auto
+    make_setter(SetterFunction&& set) { return Setter<T, SetterFunction> { etl::forward<SetterFunction>(set) }; }
+
+    template <typename T, typename GetterFunction, typename SetterFunction> constexpr auto
+    make_getter_setter(GetterFunction&& get, SetterFunction&& set) { 
+        return GetterSetter<T, GetterFunction, SetterFunction> { 
+            etl::forward<GetterFunction>(get), 
+            etl::forward<SetterFunction>(set) 
+        }; 
+    }
+
+}
+
+#endif
---
+++ test/getter_setter.cpp
@@ -0,0 +1,68 @@
+#include "etl/getter_setter.h"
+#include "etl/placeholder.h"
+#include "gtest/gtest.h"
+#include "etl/keywords.h"
+
+using namespace Project;
+using namespace Project::etl;
+
+
+TEST(GetterSetter, Get) {
+    float value = 10;
+    val g = make_getter<float>([&value] { return value; });
+
+    float get_value = int(g);
+    const bool l = is_convertible_v<float, float>;
+    EXPECT_EQ(get_value, 10);
+    EXPECT_EQ(get_value, value);
+}
+
+TEST(GetterSetter, Set) {
+    float value = 10;
+    val g = make_setter<float>([&value] (float new_value) { value = new_value; });
+
+    g = 20;
+    EXPECT_EQ(value, 20);
+}
+
+TEST(GetterSetter, GetSet) {
+    float value = 10;
+    val g = make_getter_setter<float>([&value] { return value; }, [&value] (float new_value) { value = new_value; });
+
+    g = 20;
+    val get_value = float(g);
+    EXPECT_EQ(get_value, 20);
+    EXPECT_EQ(get_value, value);
+}
+
+TEST(GetterSetter, Class) {
+    struct Data {
+        int i; float f; bool b;
+    };
+
+    class Process {
+        Data& data;
+
+    public:
+        Process(Data& data) : data(data) {}
+
+        GetterSetter<int, std::function<int()>, std::function<void(int)>> i = {
+            [this] { return data.i; },
+            [this] (int value) { data.i = value; }
+        }; 
+
+        Getter<float, std::function<float()>> f = {
+            [this] { return data.f; }
+        }; 
+        
+        Setter<bool, std::function<void(bool)>> b = {
+            [this] (bool value) { data.b = value; }
+        }; 
+    };
+
+    Data data { 1, 0.1, false }; // data is private
+    Process process { data }; // data getter and setter
+
+    process.b = true; // more readable than `data.set_b(true)`
+    int i_now = process.i; // more readable than `int i_now = data.get_i()`
+}
