fa4cde46c4988820c3721d3e46ee07621bd8ba5e
aufam <aufamuhammad715@gmail.com>
2023-09-15 13:32:24 +0700

    adding extra keywords

--- include/etl/keywords.h
+++ include/etl/keywords.h
@@ -14,6 +14,38 @@
 #include <ciso646>       // or, and, not, not_eq
 
 
+#ifdef ETL_ENABLE_EXTRA_KEYWORDS
+
+#include "etl/getter_setter.h"
+#include "etl/function.h"
+
+#define ETL_SETUP_GETTER_SETTER(CLASS)                                                  \
+    template <typename T>                                                               \
+    using Getter = Project::etl::Getter<T, Project::etl::Function<T(), CLASS*>>;        \
+                                                                                        \
+    template <typename T>                                                               \
+    using Setter = Project::etl::Setter<T, Project::etl::Function<void(T), CLASS*>>;    \
+                                                                                        \
+    template <typename T>                                                               \
+    using GetterSetter = Project::etl::GetterSetter<T, Project::etl::Function<T(), CLASS*>, Project::etl::Function<void(T), CLASS*>>;
+
+#define constructor         // for class constructor
+#define implicit            // for implicit conversion
+#define empty_constructor   // for empty constructor
+#define copy_constructor    // for copy constructor
+#define move_constructor    // for move constructor
+#define destructor ~        // for class destructor
+#define abstract            // for a class that is not intended to be instantiated on its own but serves as a blueprint or template for other classes
+#define extendable          // for a class that can be instantiated and inherited from
+#define final               // for a class that is not intended to be inherited from
+#define extends : public    // inherits public member of other class
+
+#define namespace_start(name) namespace Project::name {
+#define namespace_end }
+
+#endif // ETL_ENABLE_EXTRA_KEYWORDS
+
+
 #ifndef ETL_DISABLE_MORE_KEYWORDS
 
 #define in :            // python's in (only for iterating a container)
@@ -30,4 +62,5 @@
 #define _ dummy__[[maybe_unused]] // unused variable
 
 #endif // ETL_DISABLE_MORE_KEYWORDS
+
 // #endif //ETL_KEYWORDS_H
