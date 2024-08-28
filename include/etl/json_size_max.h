14213b17ad23fad61d0760af752737bb2fe0e382
aufam <aufamuhammad715@gmail.com>
2024-08-28 14:04:50 +0700

    fix floating point

--- include/etl/json_size_max.h
+++ include/etl/json_size_max.h
@@ -2,6 +2,7 @@
 #define ETL_JSON_SIZE_MAX_H
 
 #include "etl/json_types.h"
+#include <cmath>
 
 namespace Project::etl::json {
     template <typename T>
@@ -19,16 +20,24 @@ namespace Project::etl::json {
         }
         else if constexpr (etl::is_floating_point_v<T>) {
             size_t scale;
+            size_t decimal;
             if constexpr (etl::is_same_v<T, float>) {
                 scale = 100;
+                decimal = 2;
             }
             else if constexpr (etl::is_same_v<T, double>) {
                 scale = 1000;
+                decimal = 3;
             }
             else {
                 scale = 10000;
+                decimal = 4;
             }
-            return size_max(ssize_t(value * scale)) + 1 + (std::abs(value) < 1.0);
+
+            if (std::abs(value) < 1.0) {
+                return decimal + 2 + std::signbit(value);
+            }
+            return size_max(ssize_t(value * scale)) + 1;
         }
         else if constexpr (etl::is_same_v<T, const char*>) {
             return ::strlen(value) + 2;
